7d58462c962c148ed4f60db3ea7cd03f6c82b6d8
Thiago Leite <leite.tjs@gmail.com>
2025-05-06 21:33:06 -0300

    feat: textarea story

---
+++ packages/docs/src/stories/TextArea.stories.tsx
@@ -0,0 +1,32 @@
+import { Box, Text, TextArea, TextAreaProps } from '@thiagoleite-ui/react';
+import { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  title: 'Form/Text Area',
+  component: TextArea,
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text size="sm">Observations:</Text>
+          {Story()}
+        </Box>
+      );
+    },
+  ],
+} as Meta<TextAreaProps>;
+
+export const Primary: StoryObj<TextAreaProps> = {
+  args: {
+    placeholder: 'Add any observation...',
+  },
+};
+
+export const Disabled: StoryObj<TextAreaProps> = {
+  args: {
+    disabled: true,
+  },
+};
