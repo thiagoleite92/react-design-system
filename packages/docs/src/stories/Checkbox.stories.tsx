e41af3b587bf7166e0df5a1ce46b2f4bf4ed7ce4
Thiago Leite <leite.tjs@gmail.com>
2025-05-06 21:32:56 -0300

    feat: checkbox story

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,22 @@
+import { Box, Text, Checkbox, CheckboxProps } from '@thiagoleite-ui/react';
+import { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  title: 'Form/Checkbox',
+  component: Checkbox,
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'row', gap: '$2' }}
+        >
+          {Story()}
+          <Text size="sm">Accept terms of use</Text>
+        </Box>
+      );
+    },
+  ],
+} as Meta<CheckboxProps>;
+
+export const Primary: StoryObj<CheckboxProps> = {};
